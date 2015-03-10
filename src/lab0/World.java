b73dca4bc693c915a04afce1d2033b9419ad0ca4
mkausalik3 <mkausalik3@L11706042769.wctc.msft>
2015-03-10 15:34:01 -0500

    Rough code

---
+++ src/lab0/World.java
@@ -0,0 +1,8 @@
+package lab0;
+
+// keeping these in a separate class is a good idea
+public class World {
+    
+    public static final String MAX_VACATION_ERR_MSG = "Sorry, daysVacation must be between 0 and maxVacationDays";
+    
+}
